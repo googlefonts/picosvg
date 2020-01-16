0725967e0742bed3573b08dcc3b7152b1028ad08
Rod Sheeter <rsheeter@google.com>
2020-01-16 11:09:58 -0800

    minimal setup now uses setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
-"""Just enough to enable local install."""
-from distutils.core import setup
+"""Just a toy, enough setuptools to be able to install.
+"""
+from setuptools import setup, find_packages
+setup(
+    name="nanosvg",
+    version="0.1",
+    packages=find_packages(),
+    scripts=["nanosvg.py"],
 
-setup(name='svg_ops',
-      version='0.1',
-      py_modules=['svg_ops'],
+    install_requires=[
+      "lxml>=4.0",
+      "skia-pathops>=0.2",
+    ],
+
+    # metadata to display on PyPI
+    author="Rod S",
+    author_email="rsheeter",
+    description=("Exploratory utility for svg simplification, "
+                 "meant for use playing with COLR fonts"),
 )
